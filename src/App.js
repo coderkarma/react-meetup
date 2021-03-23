534248db48fdeb72dd1497f1a45cc5241e0ff918
Karma Drukpa <karmadrukpa11@gmail.com>
2021-03-22 23:50:23 -0700

    Add navigation and react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      <h1>React Meetup</h1>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<MainNavigation />
+			<Switch>
+				<Route path='/' exact>
+					<AllMeetupsPage />
+				</Route>
+				<Route path='/new-meetup'>
+					<NewMeetupPage />
+				</Route>
+				<Route path='/favorites'>
+					<FavoritesPage />
+				</Route>
+			</Switch>
+		</div>
+	);
+};
 
 export default App;
