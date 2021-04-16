af9ca61b336a88b353370294f1fe4135d1e1cc6d
Karma Drukpa <karmadrukpa11@gmail.com>
2021-04-15 23:50:26 -0700

    Add Firebase database and Http POST method

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,22 @@ import React from 'react';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
+    const addMeetupHandler = (meetupData) => {
+        fetch(
+            'https://react-getting-started-d5bf5-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+    };
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 };
