be40323baa0c917062ff16826468a019e5847759
Karma Drukpa <karmadrukpa11@gmail.com>
2021-04-23 00:00:34 -0700

    wrapped index.js with favoritecontextprovider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root')
+    <React.StrictMode>
+        <FavoritesContextProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </FavoritesContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
